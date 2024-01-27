891f4ba81663f67a181ff8c6fd99e92dd005f401
Pritam <pritamnagarp2@gmail.com>
2024-01-27 10:42:07 +0530

    e

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     
     <Header/>
     <div className='flex h-4/6  flex-col w-full  mr-9  sm:mt-24 mt-24  sm:mx-44 '>
-       <p className=' sm:text-2xl  sm:mr-80 text-white'>MediaX360 works to build strong and truthful
+       <p className=' sm:text-2xl  m-16 sm:mr-80 text-white'>MediaX360 works to build strong and truthful
 relationships between  
        brands, creators and the
 consumers. Believing and standing with 
@@ -23,7 +23,7 @@ Content for all!</p>
         
       
     </div>
-    <div  className='flex w-full justify-center my-3'>  <a target='_blank' href="#" className="inline-flex items-center justify-center px-2 mx-2 py-2 text-sm font-medium text-center text-white  rounded-lg  border-2 border-cyan-400 hover:bg-pink-500">
+    <div  className='flex w-full justify-center '>  <a target='_blank' href="#" className="inline-flex items-center justify-center px-2 mx-2 py-2 text-sm font-medium text-center text-white  rounded-lg  border-2 border-cyan-400 hover:bg-pink-500">
         Know More  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg></a></div> 
